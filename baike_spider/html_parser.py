50ca444d81d0b397564e2d8a0fb683d2e01ddb5b
Gallon-PC\Gallon <gallonyin@163.com>
2017-02-22 23:34:17 +0800

    optimize parser

--- baike_spider/html_parser.py
+++ baike_spider/html_parser.py
@@ -24,7 +24,14 @@ class HtmlParser(object):
         res_data['url'] = page_url
 
         # <dd class="lemmaWgt-lemmaTitle-title"> <h1>Python</h1>
-        title_node = soup.find('dd', class_="lemmaWgt-lemmaTitle-title").find("h1")
+        title_node = soup.find('dd', class_="lemmaWgt-lemmaTitle-title")
+        if title_node == None:
+            res_data['title'] = ''
+            res_data['summary'] = ''
+            return res_data
+
+        title_node = title_node.find("h1")
+
         res_data['title'] = title_node.get_text()
 
         # <div class="lemma-summary" label-module="lemmaSummary">
@@ -32,7 +39,6 @@ class HtmlParser(object):
         res_data['summary'] = summary_node.get_text()
 
         return res_data
-
 
     def parse(self, page_url, html_cont):
         if page_url is None or html_cont is None:
